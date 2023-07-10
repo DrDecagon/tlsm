7a7a4a3edaa486c1926e726d8a8e0e58a2a40c81
DrDecagon <DrDecagon@users.noreply.github.com>
2023-07-10 11:42:31 -0300

    find hwmon pmw file

--- fan-ctl-alt.sh
+++ fan-ctl-alt.sh
@@ -2,7 +2,15 @@
 # set fan pwm speed in a linear fasion based on current temp
 # starting at 47C up to the throttle temp of 70C
 
-fanpwm=/sys/class/hwmon/hwmon0/pwm1
+count=0
+while [ $count -lt 100 ]; do
+  ffl="/sys/class/hwmon/hwmon"$count"/pwm1"
+  if test -f $ffl; then
+    fanpwm=$ffl                                # fan pwm writeout
+    break
+  fi
+  (( $count++ ))
+done
 
 while :; do
   curtemp=$(cat /sys/class/thermal/thermal_zone0/temp) # temp in millidegrees C
