7e1b7a0346065ba61c604a8bf6b8181400d721f3
DrDecagon <decagon@disroot.org>
2023-07-10 06:10:37 +0100

    changes to some values / made var default

--- fan-ctl-lin.sh
+++ fan-ctl-lin.sh
@@ -2,12 +2,14 @@
 # set fan pwm speed in a linear fasion based on current temp
 # starting at 47C up to the throttle temp of 70C
 
+fanpwm=/sys/class/hwmon/hwmon0/pwm1
+
 while :; do
-  curtemp=$(cat /sys/class/thermal/thermal_zone0/temp)     # temp in millidegrees C
-  if (( $curtemp > 46000 )); then                          # if temp greater than 46C
-    echo $(( ($curtemp - 46000) / 200 + 60 )) | sudo tee /sys/class/hwmon/hwmon0/pwm1
+  curtemp=$(cat /sys/class/thermal/thermal_zone0/temp) # temp in millidegrees C
+  if [[ $curtemp -gt 46000 ]]; then                    # if temp greater than 46C
+    echo $(( ($curtemp - 46000) / 200 + 65 )) | tee $fanpwm
   else
-    echo 0 | sudo tee /sys/class/hwmon/hwmon0/pwm1         # else shut off fan
+    echo 0 | tee $fanpwm                               # else shut off fan
   fi
-  sleep 10                                                 # wait 10 seconds
+  sleep 10                                             # wait 10 seconds
 done
