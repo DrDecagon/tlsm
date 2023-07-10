645c8210d3a0ad0ff933181b853e0a3e7f2cb68c
DrDecagon <DrDecagon@users.noreply.github.com>
2023-07-10 12:30:00 -0300

    update temps

--- fan-ctl.sh
+++ fan-ctl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mintemp=48000                                  # fan shutoff temp in millidegrees celcius
+mintemp=49000                                  # fan shutoff temp in millidegrees celcius
 minpwm=50                                      # lowest desired pwm value
 temprange=$((70000 - $mintemp))                # max temp - min temp in millidegrees celcius
-pwmrange=$((250 - $minpwm))                    # max PMW - min PWM.  0 to 255
+pwmrange=$((255 - $minpwm))                    # max PMW - min PWM.  0 to 255
 cputherm=/sys/class/thermal/thermal_zone0/temp # thermometer readout
 
 count=0
@@ -18,7 +18,7 @@ done
 
 while true; do                                 # main loop
   curtemp=$(cat $cputherm)
-  if [[ $curtemp -gt $mintemp ]]; then         # if current temp is greater than minimum
+  if [[ $curtemp -ge $mintemp ]]; then         # if current temp is greater than minimum
     echo $((($curtemp - $mintemp) * $pwmrange / $temprange + $minpwm)) | sudo tee $fanpwm
   else
     echo 0 | sudo tee $fanpwm                  # else shut off fan
