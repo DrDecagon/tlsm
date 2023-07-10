8ea4de564b54900e11134d78110c87a52db19022
DrDecagon <decagon@disroot.org>
2023-07-10 15:39:21 +0100

    updated fan-ctl to find file first

--- fan-ctl.sh
+++ fan-ctl.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-cputherm=/sys/class/thermal/thermal_zone0/temp # thermometer readout
-fanpwm=/sys/class/hwmon/hwmon0/pwm1            # fan pwm writeout
-mintemp=48000                                  # fan shutoff temp
+mintemp=48000                                  # fan shutoff temp in millidegrees celcius
 minpwm=50                                      # lowest desired pwm value
-temprange=$((70000 - $mintemp))                # in millidegrees celcius
-pwmrange=$((250 - $minpwm))                    # 0 to 255 representing 0 to 5 volts
+temprange=$((70000 - $mintemp))                # max temp - min temp in millidegrees celcius
+pwmrange=$((250 - $minpwm))                    # max PMW - min PWM.  0 to 255
+cputherm=/sys/class/thermal/thermal_zone0/temp # thermometer readout
 
-while true; do
+count=0
+while [ $count -lt 100 ]; do
+  ffl="/sys/class/hwmon/hwmon"$count"/pwm1"
+  if test -f $ffl; then
+    fanpwm=$ffl                                # fan pwm writeout
+    break
+  fi
+  (( $count++ ))
+done
+
+while true; do                                 # main loop
   curtemp=$(cat $cputherm)
   if [[ $curtemp -gt $mintemp ]]; then         # if current temp is greater than minimum
     echo $((($curtemp - $mintemp) * $pwmrange / $temprange + $minpwm)) | sudo tee $fanpwm
-  else                                         # else shut off fan
-    echo 0 | sudo tee $fanpwm
+  else
+    echo 0 | sudo tee $fanpwm                  # else shut off fan
   fi
   sleep 6                                      # wait 6 seconds
 done
